a508bc8f0263043ee2741f8cf3e5a9c956422f8a
jsmestad <justin.smestad@gmail.com>
2009-04-07 16:51:11 -0600

    updated shortcuts

---
+++ justinsmestad.el
@@ -0,0 +1,163 @@
+;; DESCRIPTION: topfunky settings
+
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+;; Save backups in one place
+;; Put autosave files (ie #foo#) in one place, *not*
+;; scattered all over the file system!
+(defvar autosave-dir
+  (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+(make-directory autosave-dir t)
+
+(defun auto-save-file-name-p (filename)
+  (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+(defun make-auto-save-file-name ()
+  (concat autosave-dir
+          (if buffer-file-name
+              (concat "#" (file-name-nondirectory buffer-file-name) "#")
+            (expand-file-name
+             (concat "#%" (buffer-name) "#")))))
+
+;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+;; list contains regexp=>directory mappings; filenames matching a regexp are
+;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+(setq backup-directory-alist (list (cons "." backup-dir)))
+
+;; Snippets
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/yasnippet.el"))
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory (concat dotfiles-dir "/vendor/yasnippet.el/snippets"))
+
+;; Commands
+(require 'unbound)
+
+;; Clojure
+
+(setq clojure-src-root "/Users/topfunky/tmp/clojure")
+(eval-after-load 'clojure-mode '(clojure-slime-config))
+
+
+;; Minor Modes
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/textmate.el"))
+(require 'textmate)
+(textmate-mode)
+(require 'whitespace)
+
+;; Major Modes
+
+;; Javascript
+;; TODO javascript-indent-level 2
+
+;; Rinari
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/jump.el"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
+(require 'rinari)
+(define-key rinari-minor-mode-map [(control meta shift down)] 'rinari-find-rspec)
+(define-key rinari-minor-mode-map [(control meta shift left)] 'rinari-find-controller)
+(define-key rinari-minor-mode-map [(control meta shift up)] 'rinari-find-model)
+(define-key rinari-minor-mode-map [(control meta shift right)] 'rinari-find-view)
+
+(autoload 'applescript-mode "applescript-mode" "major mode for editing AppleScript source." t)
+(setq auto-mode-alist
+      (cons '("\\.applescript$" . applescript-mode) auto-mode-alist))
+
+
+(require 'textile-mode)
+(add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+
+(require 'haml-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(define-key haml-mode-map [(control meta down)] 'haml-forward-sexp)
+(define-key haml-mode-map [(control meta up)] 'haml-backward-sexp)
+(define-key haml-mode-map [(control meta left)] 'haml-up-list)
+(define-key haml-mode-map [(control meta right)] 'haml-down-list)
+
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+
+(add-to-list 'auto-mode-alist '("\\.sake\\'" . ruby-mode))
+
+;; XCODE
+(require 'objc-c-mode)
+(require 'xcode)
+(define-key objc-mode-map [(meta r)] 'xcode-compile)
+(define-key objc-mode-map [(meta K)] 'xcode-clean)
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (local-set-key  [(meta O)] 'ff-find-other-file)))
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (local-set-key (kbd "C-c <right>") 'hs-show-block)
+            (local-set-key (kbd "C-c <left>")  'hs-hide-block)
+            (local-set-key (kbd "C-c <up>")    'hs-hide-all)
+            (local-set-key (kbd "C-c <down>")  'hs-show-all)
+            (hs-minor-mode t)))         ; Hide and show blocks
+
+;; Font
+;;(set-face-font 'default "-apple-inconsolata-medium-r-normal--20-0-72-72-m-0-iso10646-1")
+(setq default-frame-alist '((font . "-apple-Monaco-medium-normal-normal--12-*-*-*-*-*-fontset-auto1")))
+
+;; Color Themes
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
+(require 'color-theme)
+(color-theme-initialize)
+(load "themes/railscasts")
+(color-theme-railscasts)
+
+;; Functions
+
+(require 'line-num)
+
+;; Full screen toggle
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+(global-set-key (kbd "M-n") 'toggle-fullscreen)
+
+
+;; Keyboard
+
+;; Split Windows
+(global-set-key [f6] 'split-window-horizontally)
+(global-set-key [f7] 'split-window-vertically)
+(global-set-key [f8] 'delete-window)
+
+;; Some Mac-friendly key counterparts
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z") 'undo)
+
+;; Keyboard Overrides
+(define-key textile-mode-map (kbd "M-s") 'save-buffer)
+(define-key text-mode-map (kbd "M-s") 'save-buffer)
+
+(global-set-key [(meta up)] 'beginning-of-buffer)
+(global-set-key [(meta down)] 'end-of-buffer)
+
+(global-set-key [(meta shift right)] 'ido-switch-buffer)
+(global-set-key [(meta shift up)] 'recentf-ido-find-file)
+(global-set-key [(meta shift down)] 'ido-find-file)
+(global-set-key [(meta shift left)] 'magit-status)
+
+(global-set-key [(meta H)] 'delete-other-windows)
+
+(global-set-key [(meta D)] 'backward-kill-word) ;; (meta d) is opposite
+
+(global-set-key [(meta N)] 'cleanup-buffer)
+
+(global-set-key [(control \])] 'indent-rigidly)
+
+;; Other
+
+(prefer-coding-system 'utf-8)
+
+(server-start)
+
